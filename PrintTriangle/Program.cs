8d0df07018fa86547a9ab006392ca69cb8610e45
vineethrk <vineethrk@moback.com>
2022-09-16 09:48:25 +0530

    initial commit

---
+++ PrintTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PrintTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input the desired width: ");
+            int TrianleWidth = Convert.ToInt32(Console.ReadLine());
+
+            int height = TrianleWidth;
+            for (int row = 0; row < height; row++)
+            {
+                for (int column = 0; column < TrianleWidth; column++)
+                {
+                    Console.Write(num);
+                }
+
+                Console.WriteLine();
+                TrianleWidth--;
+            }
+        }
+    }
+}
